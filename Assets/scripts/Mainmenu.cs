b55fc0d0042992c112e202235f6325d15edafbd0
Husnain Shah Nawaz <husnainofficial2k5@gmail.com>
2025-10-06 13:00:35 +0500

    Initial commit for Catchup Unity game

--- Assets/scripts/Mainmenu.cs
+++ Assets/scripts/Mainmenu.cs
@@ -1,25 +1,52 @@
+// using UnityEngine;
+// using UnityEngine.SceneManagement;
+
+// public class MainMenu : MonoBehaviour
+// {
+//     // Ye function Start Button se call hoga
+//     public void PlayGame()
+//     {
+//         // "Game" ki jagah apna game scene ka exact naam likhna
+//         SceneManager.LoadScene("Game");
+//         //Debug.log("loading Game Scene...");   
+//         AdsManager.Instance.LoadInterstitialAd();
+
+
+        
+//     }
+
+//     // Agar Exit button bhi chahiye
+//     public void QuitGame()
+//     {
+//         Debug.Log("Quit pressed!");
+//         Application.Quit();
+//     }
+// }
+
+
+
+
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Ye function Start Button se call hoga
     public void PlayGame()
     {
-        // "Game" ki jagah apna game scene ka exact naam likhna
         SceneManager.LoadScene("Game");
-        //Debug.log("loading Game Scene...");   
         AdsManager.Instance.LoadInterstitialAd();
-
-
-        
     }
 
-    // Agar Exit button bhi chahiye
     public void QuitGame()
     {
         Debug.Log("Quit pressed!");
         Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
     }
 }
+
 
